d0fbf996e223c672a70f51e40e0f752f2c78827d
zenbook <mrgulin1@gmail.com>
2022-04-14 10:27:00 +0200

    Removed unnecessary files from the branch

--- pybind/build_library.sh
+++
@@ -1,9 +0,0 @@
-# This script compiles python library from pybind.cpp
-
-PYBIND_LOCATION="./pybind11/include/" 
-PYTHON_LOCATION="/home/mrgulin/miniconda3/include/python3.9/" 
-PYTHON_VERSION="python3.9" 
-# Python needs to have developer environment
-
-
-clang++ -shared -fPIC -std=c++11 -I$PYBIND_LOCATION -I$PYTHON_LOCATION `$PYTHON_VERSION -m pybind11 --includes` pybind.cpp -o Quant_NBody_accelerate.so `$PYTHON_VERSION-config --ldflags`
