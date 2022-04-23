e1c1ee52fbcce0e005151b4b9e71473006102bd2
zenbook <mrgulin1@gmail.com>
2022-04-23 17:10:47 +0200

    Last changes before upload for Pypi

--- setup.py
+++ setup.py
@@ -6,24 +6,23 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
-DESCRIPTION = 'A package that allows to generate Hamiltonians for Fermi-Hubbard models and ab-initio Hamiltonians.'
+DESCRIPTION = 'A python package for quantum chemistry/physics to manipulate many-body operators and wave functions.'
 
 setup(
     name="quantnbody",
     version=VERSION,
     author="Saad Yalouz, Martin Rafael Gulin, Sajathan Sekaran",
-    author_email="<syalouz@unistra.fr>",
+    author_email="yalouzsaad@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
+    url='https://github.com/SYalouz/QuantNBody',
+    license='',
     install_requires=['scipy', 'numpy', 'numba'],
-    keywords=['python', 'quantum chemistry', 'second quantization', 'Ab-initio Hamiltonian', 'Hubbard model', 
-              'Householder transformation', 'block Householder transformation'],
+    keywords=['quantum physics and chemistry', 'quantum many-body systems', 'exact diagonalization'],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3", 
+        "License :: OSI Approved :: Apache Software License "
     ]
 )
