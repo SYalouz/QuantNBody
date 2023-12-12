66e5ed1a2b00aea084516e3808f23ccaed037f81
saad yalouz <yalouzsaad@gmail.com>
2023-12-12 18:37:05 +0100

    bosonic and fermionic fockspace

--- quantnbody/__init__.py
+++ quantnbody/__init__.py
@@ -2,4 +2,4 @@ from . import bosonic
 from . import fermionic
 from . import hybrid_fermionic_bosonic
 from . import fermionic_fockspace
-from . import bosonic_fockspace
+from . import bosonic_fockspace 
